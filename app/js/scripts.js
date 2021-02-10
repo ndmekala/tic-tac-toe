e4498583cc7284cad47469f807d8b88332428239
Nirmal Mekala <ndmekala@icloud.com>
2021-02-10 17:28:37 -0600

    add notes, build grid

---
+++ app/js/scripts.js
@@ -0,0 +1,19 @@
+// one gameBoard… use module
+// write a function that will render the contents of the gameboard array to the webpage
+
+// one display controller… use moule
+
+// multiple players … use factories
+
+// build the funcitons that allow players to add marks to a specific spot on the board,
+// and then tie it to the DOM, lettin gplayers click on the gameboard to place their marker
+// don’t forget the logic that keeps players from playing in spots that are already taken
+// think carefully about where each bit of logic should reside. each little piece of functinaltiy should
+// be able to fit in the game, player, or gameboard objects… but take care to put them in “logical” places
+
+// build the logic that checks for when the game is over!
+// should check for 3 in a row and a tie
+
+// clean up the interface to allow players to put intheir names,
+// include a button to start/ restart the game
+// and add a display element that congratulates the winning player
