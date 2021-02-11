3ec045df4b4f7c981512d1e165a43930b85f3858
Nirmal Mekala <ndmekala@icloud.com>
2021-02-11 09:19:14 -0600

    succesfully added event listener w/i module

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,9 +1,50 @@
 // one gameBoard… use module
 // write a function that will render the contents of the gameboard array to the webpage
+var gameboardModule = (function() {
+    gridConverter = {
+        1: 'a1',
+        2: 'a2',
+        3: 'a3',
+        4: 'b1',
+        5: 'b2',
+        6: 'b3',
+        7: 'c1',
+        8: 'c2',
+        9: 'c3',
+    }
+    let c3
+    document.getElementById('c3').addEventListener('click', () => {
+        c3 = 'X'
+        gameboardModule.updateGameBoard();
+    })
+    let a1 = 'X'
+    let a2 = 'O'
+    let a3 = '!'
+    let b1 = ''
+    let b2 = ''
+    let b3 = ''
+    let c1 = ''
+    let c2 = ''
+    // let c3 = ''
+    return {
+        updateGameBoard: function() {
+            let gameboardArray = [a1, a2, a3, b1, b2, b3, c1, c2, c3];
+            for (let i = 1; i <= 9; i++) {
+                const cell = document.getElementById(gridConverter[i]);
+                cell.textContent = gameboardArray[i-1];
+            }
+        }
+    }
+})();
 
-// one display controller… use moule
+// one display controller… use module
 
 // multiple players … use factories
+const player = (name) => {
+    return {name};
+}
+
+
 
 // build the funcitons that allow players to add marks to a specific spot on the board,
 // and then tie it to the DOM, lettin gplayers click on the gameboard to place their marker
