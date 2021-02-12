019ad2f074d18db583ba50c3ba42fbd107cff24e
Nirmal Mekala <ndmekala@icloud.com>
2021-02-12 17:58:17 -0600

    building more functionality

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -10,27 +10,137 @@ var gameboardModule = (function() {
     }
 })();
 
+// multiple players … use factories
+const player = (name) => {
+    return {name};
+}
+
 // one display controller… use module
 var displayController = (function() {
     let xturn = true;
+    let twoplayer = false;
+    let playing = false;
+    let currentPlayerOne = player('…');
+    let currentPlayerTwo = player('COMPUTER');
+    let grid = [cellObj('a1'), cellObj('a2'), cellObj('a3'), cellObj('b1'), cellObj('b2'), cellObj('b3'), cellObj('c1'), cellObj('c2'), cellObj('c3')]
+    document.getElementById('playerone').textContent = currentPlayerOne.name;
+    document.getElementById('playertwo').textContent = currentPlayerTwo.name;
     const cellObj = (gridLocation) => {
         return {cell: gridLocation, content: ''}
     }
-    let grid = [cellObj('a1'), cellObj('a2'), cellObj('a3'), cellObj('b1'), cellObj('b2'), cellObj('b3'), cellObj('c1'), cellObj('c2'), cellObj('c3')]
+    // X PLAYER
+    document.getElementById('playerbtnone').addEventListener(('click'), () => {
+        if (!playing) {
+            currentPlayerOne = player(prompt('Player One, what is your name?'));
+            document.getElementById('playerone').textContent = currentPlayerOne.name;
+        }
+    })
+    // O PLAYER
+    document.getElementById('playerbtntwo').addEventListener(('click'), () => {
+        if (!playing) {
+            if (!twoplayer) {
+                twoplayer = true;
+                document.getElementById('toggle').textContent = 'One player'
+            }
+            currentPlayerTwo = player(prompt('Player Two, what is your name?'))
+            document.getElementById('playertwo').textContent = currentPlayerTwo.name;
+        }
+    })
+    // TWO PLAYER TOGGLE
+    document.getElementById('toggle').addEventListener(('click'), () => {
+        if (!playing) {
+            if (twoplayer) {
+                twoplayer = false;
+                document.getElementById('toggle').textContent = 'two player'
+                document.getElementById('playertwo').textContent = 'COMPUTER'
+    
+            } else {
+                twoplayer = true;
+                document.getElementById('toggle').textContent = 'one player'
+                currentPlayerTwo = player(prompt('Player Two, what is your name?'))
+                document.getElementById('playertwo').textContent = currentPlayerTwo.name;
+            }
+        }
+    })
+    // START AND RESTART
+    document.getElementById('start').addEventListener(('click'), () => {
+        if (!playing) {
+            playing = true;
+            document.getElementById('start').textContent = 'Restart'
+            if (twoplayer) {
+                document.getElementById('display').textContent = currentPlayerOne.name + '’s Turn';
+            }
+        } else {
+            playing = false;
+            xturn = true;
+            for (let i=0; i < 9; i++) {
+                grid[i].content = '';
+            }
+            document.getElementById('start').textContent = 'Start';
+            document.getElementById('display').textContent = '';
+            gameboardModule.updateGameBoard();
+        }
+    })
+    // BOX EVENT LISTENERS
     for (let i =1; i <=9; i++) {
         document.getElementById(grid[i-1].cell).addEventListener(('click'), () => {
-            if (!grid[i-1].content) {
-                if (xturn) {
-                    grid[i-1].content = 'X'
-                    xturn = false;
+            if (playing) {
+                if (!grid[i-1].content) {
+                    if (xturn) {
+                        grid[i-1].content = 'X'
+                        console.log(testGrid())
+                        if (twoplayer) {
+                            xturn = false;
+                            document.getElementById('display').textContent = currentPlayerTwo.name + '’s Turn';
+                        } else {
+                            let random
+                            while (true) {
+                                random = Math.round(Math.random()*8);
+                                if (!grid[random].content) break;
+                            }
+                            grid[random].content = 'O'
+                            console.log(testGrid())
+                        }
+                    }
+                    else {
+                        grid[i-1].content = 'O'
+                        console.log(testGrid())
+                        xturn = true;
+                        document.getElementById('display').textContent = currentPlayerOne.name + '’s Turn';
+                    }
                 }
-                else {
-                    grid[i-1].content = 'O'
-                    xturn = true;
-                }
+                gameboardModule.updateGameBoard();
             }
-            gameboardModule.updateGameBoard();
         })
+    }
+    function testGrid() {
+        if (grid[0].content && grid[0].content === grid[1].content && grid[1].content === grid[2].content) {
+            return grid[0].content;
+        }
+        else if (grid[3].content && grid[3].content === grid[4].content && grid[4].content === grid[5].content) {
+            return grid[3].content;
+        }
+        else if (grid[6].content && grid[6].content === grid[7].content && grid[7].content === grid[8].content) {
+            return grid[6].content;
+        }
+        else if (grid[0].content && grid[0].content === grid[3].content && grid[3].content === grid[6].content) {
+            return grid[0].content;
+        }
+        else if (grid[1].content && grid[1].content === grid[4].content && grid[4].content === grid[7].content) {
+            return grid[1].content;
+        }
+        else if (grid[2].content && grid[2].content === grid[5].content && grid[5].content === grid[8].content) {
+            return grid[2].content;
+        }
+        else if (grid[0].content && grid[0].content === grid[4].content && grid[4].content === grid[8].content) {
+            return grid[0].content;
+        }
+        else if (grid[2].content && grid[2].content === grid[4].content && grid[4].content === grid[6].content) {
+            return grid[2].content;
+        }
+        else {
+            return false
+        }
     }
     return {
         display: grid
